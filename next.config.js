5206b63b27beb1d7428bedff76e8b9efd45c3ccb
Jamie <jamie@Jamies-MacBook-Pro.local>
2025-04-09 09:38:10 +0100

    Build: Ignore ESLint and TypeScript errors during Vercel build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    domains: ['hzljyueohjcdgnhdlnwr.supabase.co', 'yakkt.com', 'localhost'],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
